3522735743c278cb87cb3fb71aa93094fe2e3e78
lautaguirre <32684961+lautaguirre@users.noreply.github.com>
2017-12-12 23:10:36 -0300

    contact email working

--- scripts/contactemail.php
+++ scripts/contactemail.php
@@ -1,16 +1,16 @@
 <?php
-    $scomment=strip_tags($_POST['ccomment']);
-    $semail=strip_tags($_POST['cemail']);
-    $sname=strip_tags($_POST['cname']);
+    $sname=strip_tags($_POST['name']);
+    $semail=strip_tags($_POST['email']);
+    $smessage=strip_tags($_POST['message']);
 
-    $to = 'villamartinaconsultorios@gmail.com';
+    $to = 'lauta.nahuel94@gmail.com';
     $subject = "CONTACTO";
     $message = "<html>
     <body>
         <h3>".$sname." quiere contactarse con usted y le envia el siguiente mensaje:</h3>   
         <p>Email: ".$semail."</p>
-        <p>Mensaje: ".$scomment."</p>
-        <A>https://www.villamartinarosario.com</A>
+        <p>Mensaje: ".$smessage."</p>
+        <A>http://www.lautaguirre.xyz</A>
     </body>
     </html>";
     $headers = "MIME-Version: 1.0" . "\r\n";
